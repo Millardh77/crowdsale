1433eeaa557f045cd1785d27cad29051a43bc6a9
Millard Harris <millard@freemountaininc.com>
2023-06-14 21:00:23 -0700

    Load Accounts

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,49 @@
+import { useEffect, useState } from 'react';
 import { Container } from 'react-bootstrap';
+import { ethers } from 'ethers'
 
 // Components
 import Navigation from './Navigation';
+import Info from './Info';
 
 function App() {
-    return(
+    const [provider, setProvider] = useState(null)
+    const [account, setAccount] = useState(null)
+
+    const [accountBalance, setAccountBalance] = useState(0)
+    const [isLoading, setIsLoading] = useState(true)
+
+    const loadBlockchainData = async () => {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        setProvider(provider)
+
+        // Fetch account
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        const account = ethers.utils.getAddress(accounts[0])
+        setAccount(account)
+
+        // // Fetch account balance
+        // const accountBalance = ethers.utils.formatUnits(await token.balanceOf(account), 18)
+        // setAccountBalance(accountBalance)
+
+
+        setIsLoading(false)
+    }
+
+    useEffect(() => {
+        if (isLoading) {
+          loadBlockchainData()
+        }
+      }, [isLoading])
+
+      return(
         <Container>
             <Navigation />
+            <hr />
+            {account && (
+            <Info account={account} />
+        )}
+
         </Container>
     )
 }
